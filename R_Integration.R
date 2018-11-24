d6fc375d2a7fc3aa4783afd5f7d53c76ce4d5955
emmalouiser <ruaneem@tcd.ie>
2018-11-24 13:03:04 +0000

    Getting a list of 100 GitHub users to use in my data visualisations.

--- R_Integration.R
+++ R_Integration.R
@@ -1,56 +1,69 @@
+#Interrogate the GitHub API to build visualisation of data available tht elucidates some 
+#aspect of the softare engineering process, such as a social graph of developers and projects,
+#or a visualisation of indiviudal of team performance. Provide a visualisation of this using
+#the d3js library.
+
 #install.packages("jsonlite")
-library(jsonlite)
-#install.packages("httpuv")
-library(httpuv)
 #install.packages("httr")
+#install.packages("httpuv")
+#install.packages("plotly")
+#install.packages('devtools')
+library(plotly)
+library(jsonlite)
+library(httpuv)
 library(httr)
-install.packages("plotly")
-install.packages('devtools')
+library(devtools)
+library(magrittr)
+library(dplyr)
+detach(package:plotly, unload=TRUE)
 
 oauth_endpoints("github")
-
 myapp <- oauth_app(appname = "ELR_Assignment",
                    key = "6b4cef7d873677784b87",
                    secret = "89644daf2f6c53c9f60cfb4402221a3a49b02897")
-
-# oauth credentials
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
-
-# use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
-
-# take action on http error
+req <- GET("https://api.github.com/users/emmalouiser/repos", gtoken)
+req <- GET("https://api.github.com/users/emmalouiser/following", gtoken)
 stop_for_status(req)
 
-# extract content from a request
 json1 = content(req)
-
-# convert to a data.frame
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+gitDF$login
 
-# subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
-
+gitDF[gitDF$full_name == "emmalouiser/Data_Visualisation", "created_at"]
 
 # Code above has been sourced from the following website:
 #https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
+getFollowers <- function(username)
+{
+  URL <- paste("https://api.github.com/users/", username , "/followers", sep="")
+  followers = fromJSON(URL)
+  return (followers$login)
+}
 
-#Interrogate the API - my profile 
-myData = fromJSON("https://api.github.com/users/emmalouiser")
-myData$followers
-myData$public_repos
+getFollowing <- function(username)
+{
+  URL <- paste("https://api.github.com/users/", username , "/following", sep="")
+  followers = fromJSON(URL)
+  return (followers$login)
+}
 
-myFollowers = fromJSON("https://api.github.com/users/emmalouiser/followers")
-myFollowers$login
-length(myFollowers$login)
+getRepositories <- function(username)
+{
+  URL <- paste("https://api.github.com/users/", username , "/repos", sep="")
+  repos = fromJSON(URL) 
+  return (repos$name)
+}
 
-repos = fromJSON("https://api.github.com/users/emmalouiser/repos")
-repos$name
-repos$created_at
-
-myDataJSon = toJSON(myData, pretty = TRUE)
-myDataJSon
+myFollowers <- getFollowers('emmalouiser')
+i <- 1
+while (length(myFollowers) < 100)
+{
+  print(myFollowers)
+  myFollowers = unique(c(myFollowers, getFollowers(myFollowers[i])))
+  i <- i+1
+}
 
 
